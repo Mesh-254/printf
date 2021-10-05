672d67e1c3e1bb1f3c4c0c53e7fa0cdb6f7ccbbb
homemix <kenmutati@gmail.com>
2021-10-05 18:39:09 +0000

    edit task 0

--- _printf.c
+++ _printf.c
@@ -1,158 +1,33 @@
-#include "main.h"
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
+ * _printf - function that replicates what printf does
+ * @format: a character string
  *
- * Return: pointer to valid function or NULL
+ * Return:  the number of characters printed
  */
-static int (*check_for_specifiers(const char *format))(va_list)
+int _printf(const char *format, ...)
 {
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
+	print_type argument[] = {
+		{"c", _print_char},
+		{"s", _print_string},
+		{"%", _print_percent},
+		{"d", _print_int},
+		{"i", _print_int},
+		{"r", _print_reverse},
+		{"R", _print_rot13},
+		{"b", _print_binary},
+		{"u", _print_unsigned},
+		{"o", _print_octal},
+		{"x", _print_hex_l},
+		{"X", _print_hex_u},
 		{NULL, NULL}
 	};
+	va_list ap;
+	int count = 0;
 
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
-
-/**
- * _printf - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
-
-	if (format == NULL)
-		return (-1);
-	va_start(valist, format);
-	while (format[i])
-	{
-		for (; format[i] != '%' && format[i]; i++)
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
-	}
-	va_end(valist);
+	va_start(ap, format);
+	count = get_print(format, argument, ap);
+	va_end(ap);
 	return (count);
-=======
-#include <unistd.h>
-#include <stdio.h>
-/**
-* buffer_print - print given buffer to stdout
-* @buffer: buffer to print
-* @nbytes: number of bytes to print
-* Return: nbytes
-*/
-int buffer_print(char buffer[], unsigned int nbytes)
-{
-write(1, buffer, nbytes);
-return (nbytes);
-}
-/**
- * buffer_add - adds a string to buffer
- * @buffer: buffer to fill
- * @str: str to add
- * @buffer_pos: pointer to buffer first empty position
- *
- * Return: if buffer filled and emptyed return number of printed char
- * else 0
- */
-int buffer_add(char buffer[], char *str, unsigned int *buffer_pos)
-{
-int i = 0;
-unsigned int count = 0, pos = *buffer_pos, size = BUFFER_SIZE;
-while (str && str[i])
-{
-if (pos == size)
-{
-count += buffer_print(buffer, pos);
-pos = 0;
-}
-buffer[pos++] = str[i++];
-}
-*buffer_pos = pos;
-return (count);
-}
-/**
- * _printf - produces output according to a format
- * @format: character string
- *
- * Return: the number of characters printed excluding the null byte
- * used to end output to strings
- */
-int _printf(const char *format, ...)
-{
-va_list ap;
-unsigned int i = 0, buffer_pos = 0, count = 0;
-char *res_str, *aux, buffer[BUFFER_SIZE];
-if (!format || !format[0])
-return (-1);
-va_start(ap, format);
-aux = malloc(sizeof(char) * 2);
-while (format && format[i])
-{
-if (format[i] == '%')
-{
-res_str = treat_format(format, &i, ap);
-count += buffer_add(buffer, res_str, &buffer_pos);
-free(res_str);
-}
-else
-{
-aux[0] = format[i++];
-aux[1] = '\0';
-count += buffer_add(buffer, aux, &buffer_pos);
-}
-}
-count += buffer_print(buffer, buffer_pos);
-free(aux);
-va_end(ap);
-if (!count)
-count = -1;
-return (count);
 }
--- main.h
+++ main.h
@@ -1,141 +1,35 @@
-#ifndef _FUNCTION_PRINTF_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#define _FUNCTION_PRINTF_H_
-
-#include <stdarg.h>
-
+#include <unistd.h>
 #include <stdlib.h>
-
-#include <stddef.h>
-
-
-
-#define BUFFER_SIZE 1024
-
-
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
-
- * struct modifier - mofifier fields collection
-
- * @flags: flags field composed of ['0', ' ', '#', '+', '-']
-
- * @width: width field, positive number
-
- * @precision: precision field positive number not including '.'
-
- * or -1 for '*'
-
- * @length: length field string composed of ['h', 'l']
-
- * @specifier: specifier character can one of
-
- * ['c', 's', '%', 'd', 'i', 'b', 'u', 'o', 'x', 'X', 'S', 'p', 'r', 'R']
-
- *
-
+ * struct print - print
+ * @parameter: The operator
+ * @f: The function associated
  */
-
-typedef struct modifier
-
+typedef struct print
 {
-  
-  char *flags;
-  
-  int width;
-  
-  int precision;
-  
-  char *length;
-  
-  char specifier;
-  
-} modifier_t;
-
-
-
-void *_realloc(void *ptr, unsigned int, unsigned int);
-
-int _putchar(char c);
-
-char *print_binary(modifier_t *, va_list);
-
-char *print_unsigned_int(modifier_t *, va_list);
-
-char *print_octal(modifier_t *, va_list);
-
-char *print_hex(modifier_t *, va_list);
-
-char *print_char(modifier_t *, va_list ap);
-
-char *print_int(modifier_t *, va_list ap);
-
-char *print_string(modifier_t *modif, va_list ap);
-
-char *print_rev(modifier_t *, va_list ap);
-
-char *print_big_s(modifier_t *, va_list);
-
-char *print_pointer(modifier_t *, va_list);
-
-char *rot13(char *s);
-
-int _strlen(char *s);
-
-char *print_rot(modifier_t *, va_list ap);
-
-
-
-void free_modifier(modifier_t *);
-
-char *get_flags(const char *, unsigned int *);
-
-int get_width(const char *, unsigned int *);
-
-int get_precision(const char *, unsigned int *);
-
-char *get_length(const char *, unsigned int *);
-
-char get_specifier(const char *, unsigned int *);
-
-modifier_t *get_modifier(const char *, unsigned int *);
-
-char *treat_format(const char *, unsigned int *, va_list);
+	char *parameter;
+	int (*f)(va_list ap);
+} print_type;
 
 int _printf(const char *format, ...);
-
-char *_strcpy(char *dest, char *src);
-
-void array_rev(char *arr, int len);
-
-int int_len(int num);
-
-char *ito(int n);
-
-char *reverse(char *s);
-
-
-
-/**
-
- * struct print - multiple choice print
-
- * @f: char Type of print
-
- * @func: funct
-
- */
-
-typedef struct print
-
-{
-  
-  char f;
-  
-  char *(*func)(modifier_t *, va_list);
-  
-} t_print;
-
-
-
+int _putchar(char c);
+int _print_char(va_list ap);
+int _print_string(va_list ap);
+int _print_percent(va_list ap);
+int _print_int(va_list ap);
+int _print_reverse(va_list ap);
+int _print_rot13(va_list ap);
+int _print_unsigned(va_list ap);
+int _print_octal(va_list ap);
+int _print_binary(va_list ap);
+int _print_hex_l(va_list ap);
+int _print_hex_u(va_list ap);
+char *convert(unsigned int num, int base);
+int get_print(const char *format, print_type argument[], va_list ap);
 #endif
